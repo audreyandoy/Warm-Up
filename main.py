964ce8d0348f52c98fe8c239bc5c92801f126bc6
Audrey Andoy <audreylandoy@gmail.com>
2020-09-25 23:25:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Review
+
+# Variables
+number = 0 
+
+# Loops
+for i in range(3):
+    number += i
+    print(number) 
+
+number = 0
+
+while number < 5:
+    number += 1
+    print(f"number is {number}")
+
+    if number % 2 == 0:
+        print(f"{number} is even")
+    elif number % 2 != 0:
+        print(f"{number} is odd")
+    else:
+        print("Number is 0 or not 1-5.")
+
+
+    
+
